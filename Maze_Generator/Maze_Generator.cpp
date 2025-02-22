cfb5cc1946703ac1c68a0cfcb29f035f029b8080
Shengelia1614 <sabashengeliassh@gmail.com>
2025-02-22 19:24:57 +0400

    maze is operational

--- Maze_Generator/Maze_Generator.cpp
+++ Maze_Generator/Maze_Generator.cpp
@@ -9,11 +9,18 @@
 #include "maze_module.h"
 #include "random"
 #include <vector>
+#include <windows.h>
 
-#define Grid_Size 100
 
+//#define Maze_Size 15
 
 void generator();
+
+
+
+bool GRID[Grid_x][Grid_y];
+
+
 int main()
 {
     std::cout << "Hello World!\n";
@@ -62,13 +69,21 @@ void generator()
 	std::vector<maze_module> to_be_generated_modules;
 	to_be_generated_modules.push_back(modules[rand() % 16]);
 	std::vector<maze_module> generated_modules;
-
 	sf::RenderWindow window(sf::VideoMode(1900, 1080), "SFML works!");
 
+	to_be_generated_modules[0].module_sp.setPosition(window.getSize().x/2-Grid_Size/2, window.getSize().y/2- Grid_Size / 2);
+	GRID[Grid_x / 2][Grid_y / 2] = 1;
+	to_be_generated_modules[0].grid_position = std::pair<int, int>{ Grid_x / 2 ,Grid_y / 2 };
+
+	int grid_counter = 0;
+
+
+	
 
 	while (window.isOpen())
 	{
-		window.clear();
+		window.clear(sf::Color::Black);
+		
 
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -77,39 +92,98 @@ void generator()
 				window.close();
 		}
 
-		for (size_t i = 0; i < to_be_generated_modules.size(); i++)
-		{
-			
-			if (to_be_generated_modules[i].left) { 
-				maze_module temp = left[rand() % left.size()];;
-				temp.module_sp.setPosition(to_be_generated_modules[i].module_sp.getPosition().x- Grid_Size, to_be_generated_modules[i].module_sp.getPosition().y);
-				to_be_generated_modules.push_back(temp);
+
+		if (grid_counter < Maze_Size) {
+			int size = to_be_generated_modules.size();
+			for (size_t i = 0; i < size; i++)
+			{
 				
+
+				if (to_be_generated_modules[i].left 
+					&& GRID[to_be_generated_modules[i].grid_position.first - 1][to_be_generated_modules[i].grid_position.second]==0) {
+
+					GRID[to_be_generated_modules[i].grid_position.first - 1][to_be_generated_modules[i].grid_position.second]=1;
+
+					maze_module temp = right[rand() % right.size()];
+
+					temp.grid_position.first = to_be_generated_modules[i].grid_position.first - 1;
+					temp.grid_position.second = to_be_generated_modules[i].grid_position.second;
+
+					temp.module_sp.setPosition(to_be_generated_modules[i].module_sp.getPosition().x - Grid_Size, to_be_generated_modules[i].module_sp.getPosition().y);
+					temp.right = 0;
+					
+					to_be_generated_modules.push_back(temp);
+
+				}
+				if (to_be_generated_modules[i].right 
+					&& GRID[to_be_generated_modules[i].grid_position.first + 1][to_be_generated_modules[i].grid_position.second]==0) {
+
+					GRID[to_be_generated_modules[i].grid_position.first + 1][to_be_generated_modules[i].grid_position.second] = 1;
+
+					maze_module temp = left[rand() % left.size()];
+
+
+					temp.grid_position.first = to_be_generated_modules[i].grid_position.first + 1;
+					temp.grid_position.second = to_be_generated_modules[i].grid_position.second;
+
+					temp.module_sp.setPosition(to_be_generated_modules[i].module_sp.getPosition().x + Grid_Size, to_be_generated_modules[i].module_sp.getPosition().y);
+					temp.left = 0;
+					
+					to_be_generated_modules.push_back(temp);
+				}
+				if (to_be_generated_modules[i].down 
+					&& GRID[to_be_generated_modules[i].grid_position.first][to_be_generated_modules[i].grid_position.second + 1]==0) {
+
+					GRID[to_be_generated_modules[i].grid_position.first][to_be_generated_modules[i].grid_position.second +1] = 1;
+
+					maze_module temp = up[rand() % up.size()];
+
+
+					temp.grid_position.first = to_be_generated_modules[i].grid_position.first;
+					temp.grid_position.second = to_be_generated_modules[i].grid_position.second +1;
+
+					temp.module_sp.setPosition(to_be_generated_modules[i].module_sp.getPosition().x, to_be_generated_modules[i].module_sp.getPosition().y + Grid_Size);
+					temp.up = 0;
+					
+					to_be_generated_modules.push_back(temp);
+				}
+				if (to_be_generated_modules[i].up 
+					&& GRID[to_be_generated_modules[i].grid_position.first][to_be_generated_modules[i].grid_position.second - 1]==0) {
+
+					GRID[to_be_generated_modules[i].grid_position.first][to_be_generated_modules[i].grid_position.second -1] = 1;
+
+					maze_module temp = down[rand() % down.size()];
+
+					temp.grid_position.first = to_be_generated_modules[i].grid_position.first;
+					temp.grid_position.second = to_be_generated_modules[i].grid_position.second-1;
+
+					temp.module_sp.setPosition(to_be_generated_modules[i].module_sp.getPosition().x, to_be_generated_modules[i].module_sp.getPosition().y - Grid_Size);
+					temp.down = 0;
+					
+					to_be_generated_modules.push_back(temp);
+				}
+				
+				generated_modules.push_back(to_be_generated_modules[i]);
+				to_be_generated_modules.erase(to_be_generated_modules.begin());
+				size--;
 			}
-			if (to_be_generated_modules[i].right) {
-				maze_module temp = right[rand() % right.size()];;
-				temp.module_sp.setPosition(to_be_generated_modules[i].module_sp.getPosition().x + Grid_Size, to_be_generated_modules[i].module_sp.getPosition().y);
-				to_be_generated_modules.push_back(temp);
-			}
-			if (to_be_generated_modules[i].down) {
-				maze_module temp = down[rand() % down.size()];;
-				temp.module_sp.setPosition(to_be_generated_modules[i].module_sp.getPosition().x, to_be_generated_modules[i].module_sp.getPosition().y + Grid_Size);
-				to_be_generated_modules.push_back(temp);
-			}
-			if (to_be_generated_modules[i].up) {
-				maze_module temp = up[rand() % up.size()];;
-				temp.module_sp.setPosition(to_be_generated_modules[i].module_sp.getPosition().x, to_be_generated_modules[i].module_sp.getPosition().y - Grid_Size);
-				to_be_generated_modules.push_back(temp);
-			}
-			generated_modules.push_back(to_be_generated_modules[i]);
-			to_be_generated_modules.erase(to_be_generated_modules.begin());
 		}
+
+		for (size_t i = 0; i < generated_modules.size(); i++)
+		{
+			window.draw(generated_modules[i].module_sp);
+		}
+
+		/*window.draw(modules[1].module_sp);
+
+		modules[1].left = 0;
+
+		window.draw(modules[1].module_sp);*/
+		//std::sleep(5);
+		
+		window.display();
+		grid_counter++;
 	}
 	
-	for (size_t i = 0; i < generated_modules.size(); i++)
-	{
-		window.draw(generated_modules[i].module_sp);
-	}
-
-	window.display();
+	
 }
--- Maze_Generator/maze_module.h
+++ Maze_Generator/maze_module.h
@@ -5,38 +5,65 @@
 #include "SFML/Main.hpp"
 #include "SFML/Graphics.hpp"
 
+//std::vector<std::vector<bool>> GRID;
+
+#define Maze_Size 100
+
+#define Grid_Size 20
+#define Grid_x 1900/ Grid_Size
+#define Grid_y 1080/Grid_Size
+
 
 class maze_module
 {
 	
-
+	sf::RenderTexture *texture;
+	
 public:
 	sf::Sprite module_sp;
 	bool left, right, down, up;
+	std::pair<int, int> grid_position;
 
 	//maze_module();
 
-	maze_module(bool left, bool right, bool down, bool up, int a)
+	maze_module(bool left, bool right, bool down, bool up, int grid_size)
 		: left(left), right(right), down(down), up(up)
 	{
-		default_module_texture(a);
+		//default_module_texture(a);
+		//sf::RenderTexture texture;
+		texture = new sf::RenderTexture();
+		texture->create(grid_size, grid_size);
+		texture->clear(sf::Color::Black);
+
+
+		//texture.display();
+
+		if (left) {
+			sf::RectangleShape l(sf::Vector2f((grid_size / 2)+1, grid_size / 10)); l.setPosition(0, grid_size / 2 - l.getSize().y / 2); l.setFillColor(sf::Color::White);  texture->draw(l); texture->display();
+		}
+		if (right) {
+			sf::RectangleShape r(sf::Vector2f((grid_size / 2)+1, grid_size / 10)); r.setPosition((grid_size / 2)-1, grid_size / 2 - r.getSize().y / 2); r.setFillColor(sf::Color::White); texture->draw(r); texture->display();
+		}
+		if (down) {
+			sf::RectangleShape d(sf::Vector2f(grid_size / 10, (grid_size / 2)+1)); d.setPosition((grid_size / 2) - d.getSize().x / 2, grid_size / 2); d.setFillColor(sf::Color::White); texture->draw(d); texture->display();
+		}
+		if (up) {
+			sf::RectangleShape u(sf::Vector2f(grid_size / 10, (grid_size / 2) + 1)); u.setPosition(grid_size / 2 - u.getSize().x / 2, 0); u.setFillColor(sf::Color::White); texture->draw(u); texture->display();
+		}
+
+		texture->display();
+		//module_sp.setColor(sf::Color::Blue);
+		module_sp.setTexture(texture->getTexture());
+		module_sp.setTextureRect(sf::IntRect(0, 0, grid_size, grid_size));
 	}
 
-	void default_module_texture(int& grid_size) {
-		sf::RenderTexture texture;
-		texture.create(grid_size, grid_size);
-		texture.clear(sf::Color::Transparent);
+	/*void default_module_texture(int& grid_size) {
 		
-
-		if (left) { sf::RectangleShape l(sf::Vector2f(grid_size / 2, grid_size / 5)); l.setPosition(0, grid_size / 2 - l.getSize().y / 2); l.setFillColor(sf::Color::White); texture.draw(l); }
-		if (right) { sf::RectangleShape r(sf::Vector2f(grid_size / 2, grid_size / 5)); r.setPosition(grid_size / 2, grid_size / 2 - r.getSize().y / 2); r.setFillColor(sf::Color::White); texture.draw(r); }
-		if (down) { sf::RectangleShape d(sf::Vector2f(grid_size / 5, grid_size / 2)); d.setPosition(grid_size / 2 - d.getSize().x / 2, grid_size / 2); d.setFillColor(sf::Color::White); texture.draw(d); }
-		if (up) { sf::RectangleShape u(sf::Vector2f(grid_size / 5, grid_size / 2)); u.setPosition(grid_size / 2 - u.getSize().x / 2, 0); u.setFillColor(sf::Color::White); texture.draw(u); }
-
-		module_sp.setTexture(texture.getTexture());
-	}
-	void set_texture(sf::Texture &a) {
+	}*/
+	void set_texture(const sf::Texture & a) {
+		
 		module_sp.setTexture(a);
+		module_sp.setTextureRect(sf::IntRect(0, 0, a.getSize().x, a.getSize().y));
 	}
 
 
